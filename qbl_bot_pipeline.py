e1c07955b2b1b4a0aa0f0dd4ba520d7e811d3719
Storsorken <54906004+Storsorken@users.noreply.github.com>
2023-07-24 17:09:04 +0300

    Update qbl_bot_pipeline.py

--- qbl_bot_pipeline.py
+++ qbl_bot_pipeline.py
@@ -7,18 +7,19 @@ import yaml
 def create_pages(skillmap_path, dst_dir):
     """Main loop for creating pages from a skillmap"""
     skillmap = read_skillmap(skillmap_path)
-    for _, unit in skillmap.items():
-        unit_name = unit['unit_name']
-        questions_per_skill = int(unit['questions_per_skill'])
+    for key, unit_dict in skillmap.items():
+        if not key.startswith("unit_"):
+            continue     
+        unit_name = unit_dict['unit_name']
         page_count = 1
         while True:
             page_key = f"page-{page_count}"
-            if page_key not in unit:
+            if page_key not in unit_dict:
                 break
-            page = unit[page_key]
-            page_name = page['page_name']
-            skills = page['skills']
-            created_page_path = qbl_bot.generate_page(unit_name, page_name, skills, questions_per_skill, dst_dir)
+            page_dict = unit_dict[page_key]
+            page_name = page_dict['page_name']
+            skills = page_dict['skills']
+            created_page_path = qbl_bot.generate_page(unit_name, page_name, skills, int(skillmap["questions_per_skill"]), dst_dir, skillmap["role_description"], skillmap["course_description"])
             subprocess.run(["bash", "create_PR.sh", created_page_path], check=True)
             page_count += 1
 
